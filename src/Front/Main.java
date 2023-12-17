e03224b0488278c54ad4158bc01d16eb67744475
polanaeem1 <naeemros9@gmail.com>
2023-12-17 19:20:57 +0200

    Front

---
+++ src/Front/Main.java
@@ -0,0 +1,28 @@
+package Front;
+
+import Command.*;
+import Data.Data;
+import Inventory.Product;
+import Inventory.Staff;
+import javax.swing.*;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author Pola Naeem Roshdy
+ * @author Mina Phillip William
+ * @author Mohammed Mostafa Ahmed
+ * @author Abdullah Mahmoud Ahmed
+ *
+ * @since   2023-12-9
+ */
+
+/**
+ * This Class run the whole programme
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Run.run();
+    }
+}
